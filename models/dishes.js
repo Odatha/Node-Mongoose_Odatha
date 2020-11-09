5540daf7a3bf602ca54d60dab9ad77db52998045
Odatha <bandaraods@gmail.com>
2020-11-09 19:14:20 +0530

    used mongoose to enabble a schema

---
+++ models/dishes.js
@@ -0,0 +1,22 @@
+//schemas
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const dishSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+var dishes = mongoose.model("Dish", dishSchema);
+
+module.exports = Dishes;
